87727c509fa4c97b1a03951c3e0fd31ed3db1f7e
kooltzh <kooltzh@gmail.com>
2019-01-19 18:29:17 +0800

    added update_blockchain

---
+++ update_blockchain.py
@@ -0,0 +1,84 @@
+import requests
+
+URL = 'http://localhost:5000/msg/new'
+
+JSON = {
+    'sender': 'a',
+    'recipient': 'b',
+    'original_msg': 'hello',
+    'modified_msg': 'bello',
+    'similarity': '10'
+}
+
+try:
+    r = requests.post(url=URL, json=JSON)
+except:
+    print("failed to connect to {}".format(URL))
+    exit()
+
+print(r.content)
+
+# next
+
+JSON = {
+    'sender': 'c',
+    'recipient': 'd',
+    'original_msg': 'how are you',
+    'modified_msg': 'how are you too?',
+    'similarity': '10'
+}
+
+try:
+    r = requests.post(url=URL, json=JSON)
+except:
+    print("failed to connect to {}".format(URL))
+    exit()
+
+print(r.content)
+
+# next
+
+JSON = {
+    'sender': 'b',
+    'recipient': 'c',
+    'original_msg': 'bello',
+    'modified_msg': 'bello2',
+    'similarity': '10'
+}
+
+try:
+    r = requests.post(url=URL, json=JSON)
+except:
+    print("failed to connect to {}".format(URL))
+    exit()
+
+print(r.content)
+
+# next
+
+URL = 'http://localhost:5000/mine'
+try:
+    r = requests.get(url=URL)
+except:
+    print("failed to connect to {}".format(URL))
+    exit()
+
+print(r.content)
+
+# next
+
+URL = 'http://localhost:5000/msg/trace'
+
+JSON = {
+    'sender': 'b',
+    'recipient': 'c',
+    'msg': 'bello2'
+}
+
+try:
+    r = requests.post(url=URL, json=JSON)
+except:
+    print("failed to connect to {}".format(URL))
+    exit()
+
+print(r.content)
