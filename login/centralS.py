c41b5bdaf53c597ce8c11795ed1c84b110ac81b5
pakzan <pakzzzan@hotmail.com>
2019-01-20 03:58:37 +0800

    uploaded centralS

---
+++ login/centralS.py
@@ -0,0 +1,102 @@
+import os
+from flask import Flask, render_template, redirect, url_for, request, Response
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy  import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+
+# database location
+db_path = os.path.join(os.path.dirname(__file__), 'database.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+def sendJSON(ipAddress, JSON):
+    URL = 'http://' + ipAddress + ':5000/inter_msg'
+    try:
+        r = requests.post(url=URL, data=JSON)
+    except Exception as e:
+        print(e)
+        print("failed to connect to {}".format(URL))
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    status = db.Column(db.Boolean)
+    ipAddress = db.Column(db.String(15))
+    pubkey = db.Column(db.String(128))
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.form
+    user = User.query.filter_by(username=data['username']).first()
+    if user:
+        if check_password_hash(user.password, data['password']):
+            login_user(user, remember=data['remember'])
+
+            # update database
+            user.status = True
+            user.ipAddress = data['ipAddress']
+            db.session.commit()
+            return True
+
+    return False
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    new_user = User(username=data['username'],
+                    email=data['number'], password=data['password'],
+                    status=False, pubkey=data['pubkey'])
+    db.session.add(new_user)
+    db.session.commit()
+
+    return True
+
+@app.route('/get_rec_pub', methods=['POST'])
+def get_rec_pub():
+    data = request.form
+    #get pubkey
+    recipient = User.query.filter_by(username=data['recipient']).first()
+    if recipient:
+        rec_pubkey = recipient.pubkey
+    return rec_pubkey
+
+@app.route('/send_msg', methods=['POST'])
+def send_msg():
+    data = request.form
+    #get ip address
+    recipient = User.query.filter_by(username=data['recipient']).first()
+    if recipient:
+        rec_ipAddress = recipient.ipAddress
+        JSON = {
+            'sender': data['name'],
+            'recipient': data['rec_pubkey'],
+            'msg': data['message']
+        }
+        sendJSON(rec_ipAddress, JSON)
+        return True
+    return False
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return True
+
+if __name__ == '__main__':
+    app.run(debug=True)
