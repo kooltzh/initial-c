cf147ccb1efb0a4eeeceda32bfcd1c2ef2ce16b6
kooltzh <kooltzh@gmail.com>
2019-01-20 11:27:27 +0800

    latest

--- login/update_chat.py
+++ login/update_chat.py
@@ -6,7 +6,7 @@ URL = "http://localhost:5002/msg/send"
 data = {
     'target': 'admin2',
     'sender': 'Me',
-    'msg': 'Hello good morning',
+    'msg': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     'time': 'Today morning',
 }
 
