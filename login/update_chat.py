0eecf64a18e96d8cdd97e231a0e316e57743c856
kuroganechong <kuroganechong@gmail.com>
2019-01-20 12:51:49 +0800

    uploaded architecture image

--- login/update_chat.py
+++ login/update_chat.py
@@ -4,14 +4,12 @@ import json
 URL = "http://localhost:5002/msg/send"
 
 data = {
-    'target': 'admin2',
+    'target': 'admin3',
     'sender': 'Me',
-    'msg': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    'msg': 'Hello! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     'time': 'Today morning',
 }
 
-json_txt = json.dumps(data)
-
-r = requests.post(URL, json=json_txt)
+r = requests.post(URL, json=data)
 
 print(r.content)
